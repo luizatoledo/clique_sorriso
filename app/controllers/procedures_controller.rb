45dd465c88f61b8c8607c79a8a1e1be72400003c
shu qi <laranjeinita@gmail.com>
2020-12-01 15:09:52 -0300

    change procedure create action

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -1,14 +1,17 @@
 class ProceduresController < ApplicationController
   def new
+    raise
     @procedure = Procedure.new
+    @dentist = Dentist.find(params[:dentist_id])
   end
 
   def create
     @procedure = Procedure.new
-    @procedure.dentist = Dentist.find(params[:procedure][:dentist]) if Dentist.find(params[:procedure][:dentist]).user == current_user
+    @procedure.dentist = Dentist.find(params[:procedure][:dentist])
+    # @procedure.dentist = Dentist.find(params[:procedure][:dentist]) if Dentist.find(params[:procedure][:dentist]).user == current_user || current_user.admin?
     @procedure.service = Service.find(params[:procedure][:service])
     if @procedure.save
-      redirect_to procedure(@procedure)
+      redirect_to procedure_path(@procedure)
     else
       render :new
     end
@@ -19,7 +22,7 @@ class ProceduresController < ApplicationController
   end
 
   def show
-    @procedure = Procedure.find(params[:procedure][:dentist])
+    @procedure = Procedure.find(params[:id])
   end
 
   def index
